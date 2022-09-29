159962f8e4332fe1978801268749a4a9e57bae0e
Sounooo <SuHuan.Li@mail.citytech.cuny.edu>
2022-09-29 17:57:53 -0400

    HW#1

---
+++ HW#1/Assets/Instantiate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    int i;
+
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            Instantiate(cube, new Vector3(i * 5.0f, 0, 0), Quaternion.identity);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
